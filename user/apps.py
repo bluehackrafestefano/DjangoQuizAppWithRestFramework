d1233e945d72639d7848391921fd1532a4d21983
Rafe Stefano <stefanorafe@gmail.com>
2022-06-27 21:15:09 +0200

    imported user app from flight app

--- user/apps.py
+++ user/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
 
-class UserConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user'
+    name = 'users'
+
+    def ready(self):
+        import users.signals

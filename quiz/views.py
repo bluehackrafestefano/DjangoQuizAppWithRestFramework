81e54a39c08a59b5d50e7d0625d8a15b43b6ccd2
Rafe Stefano <stefanorafe@gmail.com>
2022-06-27 20:21:28 +0200

    added docstring to filtering on views

--- quiz/views.py
+++ quiz/views.py
@@ -12,7 +12,12 @@ class CategoryListView(generics.ListAPIView):
 class CategoryDetailView(generics.ListAPIView):
     serializer_class = CategoryDetailSerializer
     
+    # Filtering against the URL:
     def get_queryset(self):
+        """
+        This view should return a list of all the quizzes for
+        the category as determined by the category portion of the URL.
+        """
         queryset = Quiz.objects.all()
         category = self.kwargs['category']  # category from url e.g.: quiz/backend
         queryset = queryset.filter(category__name=category)
@@ -22,6 +27,10 @@ class QuizDetailView(generics.ListAPIView):
     serializer_class = QuestionSerializer
     
     def get_queryset(self):
+        """
+        This view should return a list of all the questions for
+        the title as determined by the title portion of the URL.
+        """
         queryset = Question.objects.all()
         title = self.kwargs['title']
         queryset = Question.objects.filter(quiz__title=title)

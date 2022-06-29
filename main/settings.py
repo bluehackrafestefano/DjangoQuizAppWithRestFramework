ca468127233a8d309c26ade66d35c7f9837c8fcf
Rafe Stefano <stefanorafe@gmail.com>
2022-06-29 19:40:05 +0200

    added auth settings

--- main/settings.py
+++ main/settings.py
@@ -133,3 +133,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 #     'PAGE_SIZE': 1
 # }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
+REST_AUTH_SERIALIZERS = {
+    'TOKEN_SERIALIZER': 'user.serializers.CustomTokenSerializer',
+}
+
